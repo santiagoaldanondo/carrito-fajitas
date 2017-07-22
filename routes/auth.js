2024795667580bb6525c6240030ee4dceb39be63
santiagoaldanondo <santiagoaldanondo@gmail.com>
2017-07-22 11:24:26 +0200

    models and facebook authentication

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const passport = require('passport')
+
+// Facebook authentication
+router.get('/auth/facebook', passport.authenticate('facebook'))
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  authType: 'reauthenticate',
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
+
+router.post('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+module.exports = router
