2757f923f5e14b5239edd167d06458763a393d02
santiagoaldanondo <santiagoaldanondo@gmail.com>
2017-08-18 04:52:42 +0200

    menu, navigator and javascript listeners

--- public/javascripts/listeners.js
+++ public/javascripts/listeners.js
@@ -1,12 +1,25 @@
 window.onload = function () {
-  if (document.getElementById("logout")) {
-    document.getElementById("logout").onclick = function () {
+
+  // Change <a> logout to POST method
+  if ($("#logout")) {
+    $("#logout").on("click", function (e) {
       var myForm = document.createElement("form");
       myForm.action = this.href; // the href of the link
       myForm.method = "POST";
       document.body.appendChild(myForm); // needed to prevent error "form submission cancelled because the form is not connected"
       myForm.submit();
-      return false; // cancel the actual link
-    };
+      e.preventDefault(); // cancel the link itself
+    });
   }
+
+  // Select an item from the list
+  $(".list-selectable").click(function (e) {
+    // Remove list-selected class from all the list-selectable items
+    $(".list-selectable").removeClass("list-selected");
+    // Add item-selected class to current item
+    $(e.target.closest(".list-selectable")).addClass("list-selected");
+    // Remove the prevent-default class from the menu
+    $(".prevent-default").removeClass("prevent-default");
+  });
+
 };
