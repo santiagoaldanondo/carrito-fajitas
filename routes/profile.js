f451396b5829dfcebd4428d6559784bab87b4c7f
WolfKay <haroboros@gmail.com>
2017-08-24 21:38:01 +0200

    ..

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const CATEGORIES = require("../models/food-categories");
+const multer = require("multer"); // Used to manage uploaded files
 require("dotenv").config();
+
+const upload = multer({
+  dest: process.env.UPLOAD_PATH
+}); //Set the path for the uploaded files to be saved
 
 // GET to show the user's profile
 router.get("/", (req, res, next) => {
@@ -35,12 +40,14 @@ router.get("/edit", (req, res, next) => {
 });
 
 // POST to update the user's profile
-router.post("/", (req, res, next) => {
+router.post("/", upload.single("picturePath"), (req, res, next) => {
   const userId = req.user._id;
+  const picturePath = req.file.filename ? process.env.UPLOAD_PATH + req.file.filename : null;
   const updates = {
     username: req.body.username,
     categories: req.body.categories || [],
     address: req.body.address,
+    picturePath: picturePath,
     location: {
       type: "Point",
       coordinates: [parseFloat(req.body.latitude), parseFloat(req.body.longitude)]
